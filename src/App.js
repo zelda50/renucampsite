e6cda176b57f8dfcb36a83b393a6fc2382d08efa
zelda50 <136413136+zelda50@users.noreply.github.com>
2023-10-21 18:44:23 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import './App.css';
+
+function App() {
+    return (
+        <div className='App'>
+            <Header />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='contact' element={<ContactPage />} />
+                <Route path='directory' element={<CampsitesDirectoryPage />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
