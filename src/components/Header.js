a8b5cfdf88671c54630614f721f689f09f6f949f
zelda50 <136413136+zelda50@users.noreply.github.com>
2023-10-22 15:14:50 -0700

    updated home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,11 +12,39 @@ import NucampLogo from '../app/assets/img/logo.png';
 
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
     <Navbar dark color='primary' sticky='top' expand='md'>
         <NavbarBrand href='/'>
-            <img src={NucampLogo} alt='nucamp logo' />
+            <img src={NucampLogo} alt='nucamp logo' className='float-start'/>
+            <h1 className='mt-1'>NuCamp</h1>
         </NavbarBrand>
+        <NavbarToggler onClick={() => setMenuOpen(!menuOpen)}/>
+         <Collapse isOpen={menuOpen} navbar>
+           <Nav className='ms-auto' navbar>
+            <NavItem>
+                <NavLink className='nav-link' to='/'>
+                    <i className='fa fa-home fa-lg' />Home
+                </NavLink>
+            </NavItem> 
+            <NavItem>
+                <NavLink className='nav-link' to='/directory'>
+                    <i className='fa fa-list fa-lg' />Directory
+                </NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink className='nav-link' to='/about'>
+                    <i className='fa fa-info fa-lg' />About
+                </NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink className='nav-link' to='/contact'>
+                    <i className='fa fa-address-card fa-lg' />Contact
+                </NavLink>
+            </NavItem>
+           </Nav>
+        </Collapse>
     </Navbar>
   );
 }
