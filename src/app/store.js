37afb06d7987e5f1d5d926bdb2d3137a70229f6e
zelda50 <136413136+zelda50@users.noreply.github.com>
2023-11-04 08:59:30 -0700

    updated store

--- src/app/store.js
+++ src/app/store.js
@@ -1,15 +1,16 @@
-import { commentsReducer } from '../features/comments/commentsSlice';
-import { promotionsReducer } from '../features/promotions/promotionsSlice';
-import { partnersReducer } from '../features/partners/partnersSlice';
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
+import { commentsReducer } from '../features/comments/commentsSlice';
+import { partnersReducer } from '../features/partners/partnersSlice';
+import { promotionsReducer } from '../features/promotions/promotionsSlice';
 
 export const store = configureStore({
-  reducer: {
-    campsites: campsitesReducer,
-    comments: commentsReducer,
-    partners: partnersReducer,
-    promotions: promotionsReducer
-
-    }
+    reducer: {
+        campsites: campsitesReducer,
+        comments: commentsReducer,
+        partners: partnersReducer,
+        promotions: promotionsReducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
